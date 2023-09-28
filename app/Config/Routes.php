09098b05a6902c3ec4dfa5519fb6db76bf880879
Torikul <Torikul>
2023-09-28 12:48:32 +0600

    all route are updated get post in match method

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -27,11 +27,9 @@ $routes->group('admin', ['filter' => 'isLoggedIn'], function ($routes) {
     $routes->get('/', 'Admin\Dashboard::index');
     $routes->get('dashboard', 'Admin\Dashboard::index');
     //Blog
-    $routes->get('blog_add', 'Admin\Blog::add');
-    $routes->post('blog_add', 'Admin\Blog::add');
     $routes->get('blog_list', 'Admin\Blog::index');
-    $routes->get('blog_edit/(:num)', 'Admin\Blog::edit/$1');
-    $routes->post('blog_edit/(:num)', 'Admin\Blog::edit/$1');
+    $routes->match(['get', 'post'], 'blog_add', 'Admin\Blog::add');
+    $routes->match(['get', 'post'], 'blog_edit/(:num)', 'Admin\Blog::edit/$1');
     $routes->get('blog_delete/(:num)', 'Admin\Blog::delete/$1');
     //theme
     $routes->get('theme', 'Admin\Theme::index');
@@ -39,10 +37,8 @@ $routes->group('admin', ['filter' => 'isLoggedIn'], function ($routes) {
     //profile
     $routes->get('profile', 'Admin\Profile::index');
     $routes->post('profile', 'Admin\Profile::edit');
-    $routes->get('profile_image', 'Admin\Profile::edit_profile_image');
-    $routes->post('profile_image', 'Admin\Profile::edit_profile_image');
-    $routes->get('banner_image', 'Admin\Profile::edit_banner_image');
-    $routes->post('banner_image', 'Admin\Profile::edit_banner_image');
+    $routes->match(['get', 'post'], 'profile_image', 'Admin\Profile::edit_profile_image');
+    $routes->match(['get', 'post'], 'banner_image', 'Admin\Profile::edit_banner_image');
     //contact
     $routes->get('contact', 'Admin\Contact::index');
     $routes->post('contact', 'Admin\Contact::edit');
@@ -50,106 +46,56 @@ $routes->group('admin', ['filter' => 'isLoggedIn'], function ($routes) {
     $routes->get('about', 'Admin\About::index');
     $routes->post('about', 'Admin\About::edit');
     //External Link
-    $routes->get('external_link_add', 'Admin\External_link::add');
-    $routes->post('external_link_add', 'Admin\External_link::add');
     $routes->get('external_link_list', 'Admin\External_link::index');
-    $routes->get('external_link_edit/(:num)', 'Admin\External_link::edit/$1');
-    $routes->post('external_link_edit/(:num)', 'Admin\External_link::edit/$1');
+    $routes->match(['get', 'post'], 'external_link_add', 'Admin\External_link::add');
+    $routes->match(['get', 'post'], 'external_link_edit/(:num)', 'Admin\External_link::edit/$1');
     $routes->get('external_link_delete/(:num)', 'Admin\External_link::delete/$1');
 
     //Educational Qualifications
     $routes->get('educational_qualifications', 'Admin\Educational_qualifications::list');
-
-    $routes->get('educational_qualifications_add', 'Admin\Educational_qualifications::add');
-
-    $routes->get('educational_qualifications_edit/(:num)', 'Admin\Educational_qualifications::edit/$1');
-
-    $routes->post('educational_qualifications_edit/(:num)', 'Admin\Educational_qualifications::edit/$1');
-
-    $routes->post('educational_qualifications_add', 'Admin\Educational_qualifications::add');
-
+    $routes->match(['get', 'post'], 'educational_qualifications_add', 'Admin\Educational_qualifications::add');
+    $routes->match(['get', 'post'], 'educational_qualifications_edit/(:num)', 'Admin\Educational_qualifications::edit/$1');
     $routes->get('educational_qualifications_delete/(:num)', 'Admin\Educational_qualifications::delete/$1');
 
     //Achievements and excellence
 
     $routes->get('achievements_and_excellence', 'Admin\Achievements_and_excellence::list');
-
-    $routes->get('achievements_and_excellence_add', 'Admin\Achievements_and_excellence::add');
-
-    $routes->get('achievements_and_excellence_edit/(:num)', 'Admin\Achievements_and_excellence::edit/$1');
-
-    $routes->post('achievements_and_excellence_edit/(:num)', 'Admin\Achievements_and_excellence::edit/$1');
-
-    $routes->post('achievements_and_excellence_add', 'Admin\Achievements_and_excellence::add');
-
+    $routes->match(['get', 'post'], 'achievements_and_excellence_add', 'Admin\Achievements_and_excellence::add');
+    $routes->match(['get', 'post'], 'achievements_and_excellence_edit/(:num)', 'Admin\Achievements_and_excellence::edit/$1');
     $routes->get('achievements_and_excellence_delete/(:num)', 'Admin\Achievements_and_excellence::delete/$1');
 
     //Professional Experience
     $routes->get('professional_experience', 'Admin\Professional_experience::list');
-
-    $routes->get('professional_experience_add', 'Admin\Professional_experience::add');
-
-    $routes->get('professional_experience_edit/(:num)', 'Admin\Professional_experience::edit/$1');
-
-    $routes->post('professional_experience_edit/(:num)', 'Admin\Professional_experience::edit/$1');
-
-    $routes->post('professional_experience_add', 'Admin\Professional_experience::add');
-
+    $routes->match(['get', 'post'], 'professional_experience_add', 'Admin\Professional_experience::add');
+    $routes->match(['get', 'post'], 'professional_experience_edit/(:num)', 'Admin\Professional_experience::edit/$1');
     $routes->get('professional_experience_delete/(:num)', 'Admin\Professional_experience::delete/$1');
 
     //Teaching and mentoring
-    // $routes->get('teaching_and_mentoring', 'Admin\Teaching_and_mentoring::list');
 
-    // $routes->get('teaching_and_mentoring_add', 'Admin\Teaching_and_mentoring::add');
-
-    $routes->get('teaching_and_mentoring_edit/(:num)', 'Admin\Teaching_and_mentoring::edit/$1');
-
-    $routes->post('teaching_and_mentoring_edit/(:num)', 'Admin\Teaching_and_mentoring::edit/$1');
-
-    // $routes->post('teaching_and_mentoring_add', 'Admin\Teaching_and_mentoring::add');
-
-    // $routes->get('teaching_and_mentoring_delete/(:num)', 'Admin\Teaching_and_mentoring::delete/$1');
+    $routes->match(['get', 'post'], 'teaching_and_mentoring_edit/(:num)', 'Admin\Teaching_and_mentoring::edit/$1');
 
     //Journal Publications
     $routes->get('journal_publications', 'Admin\Journal_publications::list');
-
-    $routes->get('journal_publications_add', 'Admin\Journal_publications::add');
-
-    $routes->get('journal_publications_edit/(:num)', 'Admin\Journal_publications::edit/$1');
-
-    $routes->post('journal_publications_edit/(:num)', 'Admin\Journal_publications::edit/$1');
-
-    $routes->post('journal_publications_add', 'Admin\Journal_publications::add');
-
+    $routes->match(['get', 'post'], 'journal_publications_add', 'Admin\Journal_publications::add');
+    $routes->match(['get', 'post'], 'journal_publications_edit/(:num)', 'Admin\Journal_publications::edit/$1');
     $routes->get('journal_publications_delete/(:num)', 'Admin\Journal_publications::delete/$1');
 
     //Conference Publications
     $routes->get('conference_publications', 'Admin\Conference_publications::list');
-
-    $routes->get('conference_publications_add', 'Admin\Conference_publications::add');
-
-    $routes->get('conference_publications_edit/(:num)', 'Admin\Conference_publications::edit/$1');
-
-    $routes->post('conference_publications_edit/(:num)', 'Admin\Conference_publications::edit/$1');
-
-    $routes->post('conference_publications_add', 'Admin\Conference_publications::add');
-
+    $routes->match(['get', 'post'], 'conference_publications_add', 'Admin\Conference_publications::add');
+    $routes->match(['get', 'post'], 'conference_publications_edit/(:num)', 'Admin\Conference_publications::edit/$1');
     $routes->get('conference_publications_delete/(:num)', 'Admin\Conference_publications::delete/$1');
 
     //Overall Skills
-    $routes->get('overall_skills_add', 'Admin\Overall_skills::add');
-    $routes->post('overall_skills_add', 'Admin\Overall_skills::add');
     $routes->get('overall_skills_list', 'Admin\Overall_skills::index');
-    $routes->get('overall_skills_edit/(:num)', 'Admin\Overall_skills::edit/$1');
-    $routes->post('overall_skills_edit/(:num)', 'Admin\Overall_skills::edit/$1');
+    $routes->match(['get', 'post'], 'overall_skills_add', 'Admin\Overall_skills::add');
+    $routes->match(['get', 'post'], 'overall_skills_edit/(:num)', 'Admin\Overall_skills::edit/$1');
     $routes->get('overall_skills_delete/(:num)', 'Admin\Overall_skills::delete/$1');
 
     //Language Skills
-    $routes->get('language_skills_add', 'Admin\Language_skills::add');
-    $routes->post('language_skills_add', 'Admin\Language_skills::add');
     $routes->get('language_skills_list', 'Admin\Language_skills::index');
-    $routes->get('language_skills_edit/(:num)', 'Admin\Language_skills::edit/$1');
-    $routes->post('language_skills_edit/(:num)', 'Admin\Language_skills::edit/$1');
+    $routes->match(['get', 'post'], 'language_skills_add', 'Admin\Language_skills::add');
+    $routes->match(['get', 'post'], 'language_skills_edit/(:num)', 'Admin\Language_skills::edit/$1');
     $routes->get('language_skills_delete/(:num)', 'Admin\Language_skills::delete/$1');
 
     //login system
